6e5958c6379fb5ed7447427a1df5ee9c51d08deb
kose <53458630+kokseen1@users.noreply.github.com>
2021-12-20 14:07:57 +0800

    update: improved regex accuracy

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from datetime import datetime
 import pickle
 from matplotlib import pyplot as plt
 from matplotlib import dates as mdates
+from random import randint
+import time
 
 PICKLE_FILENAME = "data_list.pkl"
 
@@ -20,17 +22,24 @@ def from_pickle():
 
 
 def get_html(url):
-    return requests.get(url).text
+    res = requests.get(url).text
+    with open(f"htmls/{time.time()}.html", "w", encoding="utf8") as f:
+        f.write(res)
+    return res
 
 
-def gsearch(query):
+def gsearch(query, results=None):
+    num = ""
+    if results:
+        num = f"&num={results}"
     return get_html(
         f"https://www.google.com/search?q={query}+carousell+site%3Ahttps%3A%2F%2Fwww.carousell.sg%2Fp%2F"
+        + num
     )
 
 
 def get_valid_urls(html):
-    return re.findall(r'<a href="/url\?q=(https://www.carousell.sg/p/.+?)/&amp;', html)
+    return re.findall(r"(https://www.carousell.sg/p/.+?-?\d{7,})", html)
 
 
 def format_date(date_str):
@@ -61,8 +70,9 @@ def gen_plot(data_list):
     ax = plt.gca()
     formatter = mdates.DateFormatter("%Y/%m/%d")
     ax.xaxis.set_major_formatter(formatter)
-    locator = mdates.DayLocator(interval=100)
+    locator = mdates.DayLocator()
     ax.xaxis.set_major_locator(locator)
+    plt.xticks(x_values)
     plt.title(data_list[0][0])
     plt.xlabel("Date")
     plt.ylabel("Price (S$)")
@@ -71,30 +81,42 @@ def gen_plot(data_list):
     plt.show()
 
 
-def get_car_data(car_urls, verbose=False):
-    if verbose:
-        print(car_urls)
+def get_car_data(car_urls, keywords=[], delay=False):
+    print(car_urls)
     data_list = []
     for url in car_urls:
+        strict_fail = any([kw.lower() not in url for kw in keywords])
+        if strict_fail:
+            print(f"Skipped {url} [STRICT MODE]")
+            continue
         html = get_html(url)
+        if delay:
+            time.sleep(randint(2, 8))
         data = get_name_date_price(html)
         if not data:
+            print(f"Skipped {url} [INVALID URL]")
             continue
         data += tuple([url])
         data_list.append(data)
-        if verbose:
-            print("{}\n{}\n${}\n{}\n".format(*data))
+        print("{}\n{}\n${}\n{}\n".format(*data))
     return data_list
 
 
-def main():
-    html = gsearch("ier m9")
-    # with open("gsearch.html", "r", encoding="utf8") as f:
-    #     html = f.read()
+def main(d=0):
+    query = "er2se"
+    strict = True
+    if d:
+        with open("gsearch.html", "r", encoding="utf8") as f:
+            html = f.read()
+        with open("car.html", "r", encoding="utf8") as f:
+            res = f.read()
+    else:
+        html = gsearch(query, results=100)
     car_urls = get_valid_urls(html)
-    # with open("car.html", "r", encoding="utf8") as f:
-    # res = f.read()
-    data_list = get_car_data(car_urls, True)
+    keywords = []
+    if strict:
+        keywords = query.split()
+    data_list = get_car_data(car_urls, keywords, delay=False)
     to_pickle(data_list)
     gen_plot(data_list)
 
